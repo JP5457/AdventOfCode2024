f9cd6c4c038a1d394beb4be1d404e4127c95ab05
James Pursglove <92054647+JP5457@users.noreply.github.com>
2024-12-11 13:59:36 +0000

    day 10 and 11

---
+++ adventOfCode/day10.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+namespace Day10
+{
+    public class Day10
+    {
+
+        static List<string> readFile(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            string line;
+            List<string> data = new List<string>();
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                data.Add(line);
+                line = sr.ReadLine();
+            }
+            return data;
+        }
+
+        static string setChar(int position, string text, char toSet)
+        {
+            char[] temp = text.ToCharArray();
+            temp[position] = toSet;
+            return new string(temp);
+        }
+
+        static int valueOfHead(List<string> data, int[] head)
+        {
+            List<string> reached = new List<string>(data);
+            reached[head[0]] = setChar(head[1], reached[head[0]], '#');
+            int score = 0;
+            bool changed = true;
+            while (changed) {
+                changed = false;
+                for (int y = 0; y < data.Count(); y++)
+                {
+                    for (int x = 0; x < data[y].Length; x++)
+                    {
+                        if (reached[y][x] == '#')
+                        {
+                            int val = int.Parse(data[y][x].ToString());
+                            if (y > 0)
+                            {
+                                if (data[y - 1][x] != '-' && reached[y - 1][x] != '#')
+                                {
+                                    if (int.Parse(data[y - 1][x].ToString()) == val + 1)
+                                    {
+                                        reached[y - 1] = setChar(x, reached[y - 1], '#');
+                                        changed = true;
+                                    }
+                                }
+                            }
+                            if (y < data.Count() - 1)
+                            {
+                                if (data[y + 1][x] != '-' && reached[y + 1][x] != '#')
+                                {
+                                    if (int.Parse(data[y + 1][x].ToString()) == val + 1)
+                                    {
+                                        reached[y + 1] = setChar(x, reached[y + 1], '#');
+                                        changed = true;
+                                    }
+                                }
+                            }
+                            if (x > 0)
+                            {
+                                if (data[y][x - 1] != '-' && reached[y][x - 1] != '#')
+                                {
+                                    if (int.Parse(data[y][x - 1].ToString()) == val + 1)
+                                    {
+                                        reached[y] = setChar(x - 1, reached[y], '#');
+                                        changed = true;
+                                    }
+                                }
+                            }
+                            if (x < data[y].Length - 1)
+                            {
+                                if (data[y][x + 1] != '-' && reached[y][x + 1] != '#')
+                                {
+                                    if (int.Parse(data[y][x + 1].ToString()) == val + 1)
+                                    {
+                                        reached[y] = setChar(x + 1, reached[y], '#');
+                                        changed = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            for (int y = 0; y < data.Count(); y++)
+            {
+                for (int x = 0; x < data[y].Length; x++)
+                {
+                    if (reached[y][x] == '#' && data[y][x] == '9')
+                    {
+                        score++;
+                    }
+                }
+            }
+            return score;
+        }
+
+
+        public static void Task1()
+        {
+            List<string> data = readFile("C:\\Users\\James\\source\\repos\\adventOfCode\\adventOfCode\\input.txt");
+            int total = 0;
+            for (int y = 0; y < data.Count(); y++)
+            {
+                while (data[y].IndexOf('0') >= 0)
+                {
+                    int x = data[y].IndexOf('0');
+                    total += valueOfHead(data, [y, x]);
+                    data[y] = setChar(x, data[y], '-');
+                }
+            }
+            Console.WriteLine(total);
+        }
+
+        static int ratingOfHead(List<string> data, int[] point)
+        {
+            int total = 0;
+            int y = point[0];
+            int x = point[1];
+            int val = int.Parse(data[y][x].ToString());
+            if (y > 0)
+            {
+                if (data[y - 1][x] != '-')
+                {
+                    if (int.Parse(data[y - 1][x].ToString()) == val + 1)
+                    {
+                        if (val == 8) { total += 1; }
+                        else
+                        {
+                            total += ratingOfHead(data, [y - 1, x]);
+                        }
+                    }
+                }
+            }
+            if (y < data.Count() - 1)
+            {
+                if (data[y + 1][x] != '-')
+                {
+                    if (int.Parse(data[y + 1][x].ToString()) == val + 1)
+                    {
+                        if (val == 8) { total += 1; }
+                        else
+                        {
+                            total += ratingOfHead(data, [y + 1, x]);
+                        }
+                    }
+                }
+            }
+            if (x > 0)
+            {
+                if (data[y][x - 1] != '-')
+                {
+                    if (int.Parse(data[y][x - 1].ToString()) == val + 1)
+                    {
+                        if (val == 8) { total += 1; }
+                        else
+                        {
+                            total += ratingOfHead(data, [y, x - 1]);
+                        }
+                    }
+                }
+            }
+            if (x < data[y].Length - 1)
+            {
+                if (data[y][x + 1] != '-')
+                {
+                    if (int.Parse(data[y][x + 1].ToString()) == val + 1)
+                    {
+                        if (val == 8) { total += 1; }
+                        else
+                        {
+                            total += ratingOfHead(data, [y, x+1]);
+                        }
+                    }
+                }
+            }
+            return total;
+        }
+
+        public static void Task2()
+        {
+            List<string> data = readFile("C:\\Users\\James\\source\\repos\\adventOfCode\\adventOfCode\\input.txt");
+            int total = 0;
+            for (int y = 0; y < data.Count(); y++)
+            {
+                while (data[y].IndexOf('0') >= 0)
+                {
+                    int x = data[y].IndexOf('0');
+                    total += ratingOfHead(data, [y, x]);
+                    data[y] = setChar(x, data[y], '-');
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ adventOfCode/day11.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+using static System.Formats.Asn1.AsnWriter;
+namespace Day11
+{
+    public class Day11
+    {
+
+        static List<string> readFile(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            string line;
+            List<string> data = new List<string>();
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                data.Add(line);
+                line = sr.ReadLine();
+            }
+            return data;
+        }
+
+        static List<Int64> blink(List<Int64> stones)
+        {
+            List<Int64> newStones = new List<Int64>();
+            foreach (Int64 stone in stones)
+            {
+                if(stone == 0)
+                {
+                    newStones.Add(1);
+                } else if ((stone.ToString().Length % 2) == 0) {
+                    newStones.Add(Int64.Parse(stone.ToString().Substring(0, (int)(stone.ToString().Length / 2))));
+                    newStones.Add(Int64.Parse(stone.ToString().Substring((int)(stone.ToString().Length / 2), (int)(stone.ToString().Length / 2))));
+                } else
+                {
+                    newStones.Add(stone * 2024);
+                }
+            }
+            return newStones;
+        }
+
+        public static void Task1()
+        {
+            List<string> data = readFile("C:\\Users\\James\\source\\repos\\adventOfCode\\adventOfCode\\input.txt");
+            int total = 0;
+            string[] stones = data[0].Split(' ');
+            List<Int64> nums = stones.Select(stone => Int64.Parse(stone)).ToList();
+            for(int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(i);
+                nums = blink(nums);
+            }
+            Console.WriteLine(nums.Count());
+        }
+
+      
+        public static void Task2()
+        {
+            List<string> data = readFile("C:\\Users\\James\\source\\repos\\adventOfCode\\adventOfCode\\input.txt");
+            Int64 total = 0;
+
+            string[] stones = data[0].Split(' ');
+            List<Int64> nums = stones.Select(stone => Int64.Parse(stone)).ToList();
+            Dictionary<Int64, Int64[]> splits = new Dictionary<Int64, Int64[]>();
+            Dictionary<Int64, Int64> stoneCount = new Dictionary<Int64, Int64>();
+            foreach (Int64 stone in nums)
+            {
+                stoneCount.Add(stone, 1);
+            }
+            if (!stoneCount.ContainsKey(1)) { stoneCount.Add(1, 0); }
+            for (int i = 0; i < 75; i++)
+            {
+                Console.WriteLine(i);
+                Dictionary<Int64, Int64> newStoneCount = new Dictionary<Int64, Int64>(stoneCount);
+                newStoneCount[1] = newStoneCount[1] + stoneCount[0];
+                newStoneCount[0] = 0;
+                foreach (Int64 stone in stoneCount.Keys)
+                {
+                    if ((stone.ToString().Length % 2) == 0)
+                    {
+                        if (splits.ContainsKey(stone))
+                        {
+                            newStoneCount[splits[stone][0]] = newStoneCount[splits[stone][0]] + stoneCount[stone];
+                            newStoneCount[splits[stone][1]] = newStoneCount[splits[stone][1]] + stoneCount[stone];
+                        }
+                        else
+                        {
+                            Int64[] split = new Int64[2];
+                            split[0] = Int64.Parse(stone.ToString().Substring(0, (int)(stone.ToString().Length / 2)));
+                            split[1] = Int64.Parse(stone.ToString().Substring((int)(stone.ToString().Length / 2), (int)(stone.ToString().Length / 2)));
+                            if (!newStoneCount.ContainsKey(split[0])) { newStoneCount.Add(split[0], 0); }
+                            if (!newStoneCount.ContainsKey(split[1])) { newStoneCount.Add(split[1], 0); }
+                            splits.Add(stone, split);
+                            newStoneCount[split[0]] = newStoneCount[split[0]] + stoneCount[stone];
+                            newStoneCount[split[1]] = newStoneCount[split[1]] + stoneCount[stone];
+                        }
+                        newStoneCount[stone] = newStoneCount[stone] - stoneCount[stone] ;
+                    }
+                    else
+                    {
+                        if (!newStoneCount.ContainsKey(stone * 2024)) { newStoneCount.Add(stone * 2024, stoneCount[stone]); } else
+                        {
+                            newStoneCount[stone * 2024] = newStoneCount[stone * 2024] + stoneCount[stone];
+                        }
+                        newStoneCount[stone] = newStoneCount[stone] - stoneCount[stone];
+                    }
+                }
+                stoneCount = newStoneCount;
+            }
+            foreach(Int64 count in stoneCount.Values)
+            {
+                total += count;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
